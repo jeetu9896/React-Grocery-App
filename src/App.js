03eba96ff0b8fa42bc668f58d5db69f636e3eebf
Jitender <58380090+jeetu9896@users.noreply.github.com>
2022-06-12 13:39:56 +0530

    Grocery App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Grocery from './Component/Grocery/Grocery';
+
+function App() {
+  return (
+    <Grocery />
+  );
+}
+
+export default App;
